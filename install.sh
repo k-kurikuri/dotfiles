70a3af927edf68fb88f53183d1d91d6480658634
k-kurikuri <kurimixgoo@gmail.com>
2018-01-18 11:07:03 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "install homebrew..."
+which brew >/dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo "brew doctor..."
+which brew >/dev/null 2>&1 && brew doctor
+
+echo "brew update..."
+which brew >/dev/null 2>&1 && brew update
+
+echo "brew upgrade..."
+brew upgrade --all
+
+# TODO: formulas....
